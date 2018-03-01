c1d5f16abe2f1e3dec78ced6d11405db1faa0a48
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-03-01 22:26:22 +0100

    Fin

---
+++ scripts.js
@@ -0,0 +1,28 @@
+/*var person = {
+    name: 'Piotr',
+    age: 25,
+    country: 'Poland'
+};
+for (var attrName in person) {
+
+    console.log(attrName);
+    console.log(person[attrName]);
+
+}
+*/
+
+function drawTree (value) {
+	for (var i = 1; i <= value; i++) {
+		var star = ''
+		for (var j = i; j < value; j++) {
+			star+= ' ';
+		}
+		for (var k = 1 ; k <= (i*2)-1; k++) {
+			star+= '*'
+		}
+		console.log(star);
+	}
+}
+
+var lol = parseFloat(prompt('podaj liczbe'));
+xd = drawTree(lol)
